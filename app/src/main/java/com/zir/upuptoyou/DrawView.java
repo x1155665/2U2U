c1050e546e3b4693fa640ee7b3abb88cee8deac9
zir <zirconium299792458@gmail.com>
2017-08-28 11:45:28 +0200

    people images loaded input text

--- app/src/main/java/com/zir/upuptoyou/DrawView.java
+++ app/src/main/java/com/zir/upuptoyou/DrawView.java
@@ -7,10 +7,13 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+
+import java.lang.reflect.Field;
 
 
 /**
@@ -25,12 +28,28 @@ public class DrawView extends View {
     final private int outputWidth = 500; //px
     final private int outputHeight = 500; //px
 
+    final int startX = 120;
+    final int startY = 40;
+    final int gapx = 50;
+    final int gapY = 22;
+    final int peopleWidth = 76;
+    final int peopleHeight = 149;
+    final int linebreakX = -52;
+    final int linebreakY = 36;
+
     Bitmap watermark_bitmap;
     Rect watermark_dst;
 
+    Bitmap person;
+    Rect person_dst;
+    String words;
+
+    Canvas canvas;
+
     public DrawView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        watermark_bitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.watermark);
+        watermark_bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.watermark);
+        words="";
     }
 
     @Override
@@ -45,12 +64,63 @@ public class DrawView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        this.canvas=canvas;
         //watermark
         canvas.drawBitmap(watermark_bitmap, null, watermark_dst, null);
+        Log.d("ondraw","canvaswidth: "+Integer.toString(canvas.getWidth()));
 
+        //draw people
+        Log.d("drawPeople", "start drawpeople");
+        int x = startX ;
+        int y = startY ;
+        int line = 0;
+        for (int i = 0; i < words.length(); i++)
+            if (words.charAt(i) != '\n') {
 
+                //load image
+                //Todo: trade ram for speed by loading all bitmaps in constructor?
+                int id = 1 + (int) (Math.random() * 25);
+                Log.d("Drawpeople","id:"+Integer.toString(id));
+                int resId;
+                if(id<10)
+                    resId= getResId("p40"+Integer.toString(id), R.drawable.class);
+                else
+                    resId= getResId("p4"+Integer.toString(id), R.drawable.class);
+                person = BitmapFactory.decodeResource(getResources(), resId);
+
+                //set location
+                x += gapx;
+                y += gapY;
+                Log.d("drawpeople",Integer.toString(i)+" : x"+ Integer.toString(x)+ " / y " + Integer.toString(y));
+                person_dst = new Rect((int) (x / (float) outputWidth * Width), (int) (y / (float) outputHeight * Height), (int) ((x + peopleWidth) / (float) outputWidth * Width), (int) ((y + peopleHeight) / (float) outputHeight * Height));
+
+                Log.d("Drawpeople","canvaswidth: "+Integer.toString(canvas.getWidth()));
+                canvas.drawBitmap(person, null, person_dst, null);
+            } else {
+                line++;
+                x = startX + line * linebreakX;
+                y = startY + line * linebreakY;
+            }
     }
 
 
+    //redraw all people
+    //// TODO: refine drawing process 
+    public void drawPeople(String words) {
+        this.words = words;
+        invalidate();
+    }
+
+
+
+    private static int getResId(String variableName, Class<?> c) {
+        try {
+            Field idField = c.getDeclaredField(variableName);
+            return idField.getInt(idField);
+        } catch (Exception e) {
+            Log.e("getResId","cant find res!!");
+            e.printStackTrace();
+            return -1;
+        }
+    }
 }
--- app/src/main/java/com/zir/upuptoyou/MainActivity.java
+++ app/src/main/java/com/zir/upuptoyou/MainActivity.java
@@ -5,27 +5,51 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected DrawView drawView;
+    protected EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawView = (DrawView) findViewById(R.id.drawView);
+        editText = (EditText) findViewById(R.id.editText);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+/*                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+
+                drawView.drawPeople("draw!\npeople");
+                drawView.setBackgroundColor(0xFFc1d540);
             }
         });
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                drawView.drawPeople(editText.getText().toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+
     }
 
     @Override
