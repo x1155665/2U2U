161de4aa1f1862576860b3320a52213e964be7c6
zir <zirconium299792458@gmail.com>
2017-09-01 02:04:29 +0200

    draw people and text in bitmap_normal, set scaled bitmap_normal to canvas in onDraw. To avoid change of people, backgroundcolor still set in drawView

--- app/src/main/java/com/zir/upuptoyou/DrawView.java
+++ app/src/main/java/com/zir/upuptoyou/DrawView.java
@@ -55,7 +55,7 @@ public class DrawView extends View {
     final int PeoplelinebreakX = -52;
     final int PeoplelinebreakY = 36;
 
-    final float TextStartX = 4f ; //
+    final float TextStartX = 4f; //
     final float TextStartY = 34.5f;
     final float TextGapX = 37.7f;
     final float TextGapY = 1.4f;
@@ -75,7 +75,6 @@ public class DrawView extends View {
     protected String words;
     protected String oldWords;
 
-    protected int[] picIds;
 
     protected Matrix matrix_text;
     protected float r_x;
@@ -84,12 +83,15 @@ public class DrawView extends View {
     Typeface fontEn;
     Typeface fontZh;
 
+    Bitmap bitmap_normal;
+    Canvas canvas_normal;
+
+
     public DrawView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         watermark_bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.watermark);
         words = "";
         oldWords = "";
-        picIds = new int[words.length()];
         paint_text = new Paint();
         paint_text.setColor(0xff40210f);
         paint_text.setFakeBoldText(true);
@@ -113,18 +115,16 @@ public class DrawView extends View {
         r_x = Width / (float) outputWidth;
         r_y = Height / (float) outputHeight;
         matrix_text = new Matrix();
-        matrix_text.setValues(new float[]{1.3f , -1.5f , 191 , 0.6f , 1 , 32 , 0, 0, 1 });
-
+        matrix_text.setValues(new float[]{1.3f, -1.5f, 191, 0.6f, 1, 32, 0, 0, 1});
+        bitmap_normal = Bitmap.createBitmap(outputWidth, outputHeight, Bitmap.Config.ARGB_8888);
+        canvas_normal = new Canvas(bitmap_normal);
+        drawNormal();
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        boolean noChange = oldWords.equals(words);
-
-        Bitmap bitmap_normal = Bitmap.createBitmap(outputWidth, outputHeight, Bitmap.Config.ARGB_8888);
-        Canvas canvas_normal = new Canvas(bitmap_normal);
+    private void drawNormal() {
+        bitmap_normal.eraseColor(Color.TRANSPARENT);
+        canvas_normal.setMatrix(new Matrix());
         //region draw in 500*500
-
         //watermark
         watermark_dst = new Rect(10, 8, (101 + 10), (8 + 45));
         canvas_normal.drawBitmap(watermark_bitmap, null, watermark_dst, null);
@@ -135,24 +135,21 @@ public class DrawView extends View {
         int y_people = PeopleStartY;
         int line = 0;
         int resId;
-        if(!noChange)
-            picIds = new int[words.length()];
+        int picId;
         for (int i = 0; i < words.length(); i++)
             if (words.charAt(i) != '\n') {
                 //load image
                 //TODO: trade ram for speed by loading all bitmaps in constructor?
-                if(!noChange)
-                    picIds[i] = 1 + (int) (Math.random() * 25);
-                if (picIds[i] < 10)
-                    resId = getResId("p40" + Integer.toString(picIds[i]), R.drawable.class);
+                picId = 1 + (int) (Math.random() * 25);
+                if (picId < 10)
+                    resId = getResId("p40" + Integer.toString(picId), R.drawable.class);
                 else
-                    resId = getResId("p4" + Integer.toString(picIds[i]), R.drawable.class);
+                    resId = getResId("p4" + Integer.toString(picId), R.drawable.class);
                 person = BitmapFactory.decodeResource(getResources(), resId);
 
                 //set location
                 x_people += PeopleGapX;
                 y_people += PeopleGapY;
-                //Log.d("drawpeople","image "+Integer.toString(i)+" : x"+ Integer.toString(x_people)+ " / y " + Integer.toString(y_people));
                 person_dst = new Rect(x_people, y_people, (x_people + PeopleWidth), (y_people + PeopleHeight));
                 canvas_normal.drawBitmap(person, null, person_dst, null);
             } else {
@@ -181,11 +178,9 @@ public class DrawView extends View {
                 if ((int) c < 128) {
                     paint_text.setTextSize(FontSizeEn);
                     paint_text.setTypeface(fontEn);
-                    //Log.d("drawText",c+"'s Size"+ String.valueOf(paint_text.getTextSize()));
                 } else {
                     paint_text.setTextSize(FontSizeZh);
                     paint_text.setTypeface(Typeface.DEFAULT_BOLD);
-                    //Log.d("drawText", c + "'s Size" + String.valueOf(paint_text.getTextSize()));
                 }
 
 /*                switch (words.charAt(i)){             //unused since ❤ and ♥ have colors in Android
@@ -197,7 +192,7 @@ public class DrawView extends View {
                         break;
                 }*/
 
-                canvas_normal.drawText(String.valueOf(words.charAt(i)).toUpperCase(), x_text , y_text , paint_text);
+                canvas_normal.drawText(String.valueOf(words.charAt(i)).toUpperCase(), x_text, y_text, paint_text);
                 //paint_text.setColor(0xff40210f);      //set color to default value if it's changed.
             } else {
                 line++;
@@ -208,24 +203,31 @@ public class DrawView extends View {
         //endregion
 
         //endregion
-        canvas.drawBitmap(bitmap_normal, null, new Rect(0,0,Width,Height), null);
-        oldWords=words;
+        oldWords = words;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(bitmap_normal, null, new Rect(0, 0, Width, Height), null);
+        Log.d("ondraw", "onDraw");
     }
 
 
-    public void refresh(){
-        for(int i = 0; i<picIds.length;i++)
-            picIds[i] = 1 + (int) (Math.random() * 25);
+    public void refresh() {
+        drawNormal();
         invalidate();
     }
+
     //redraw all people
     //TODO: refine drawing process: not all people at the same time
     public void drawPeople(String newWords) {
-        if(!words.equals(newWords)) {
+        if (!words.equals(newWords)) {
             this.words = newWords;
+            drawNormal();
             invalidate();
         }
     }
+
 
 
     //TODO: choose pic size
@@ -264,7 +266,7 @@ public class DrawView extends View {
         }
     }
 
-    public Uri share(){
+    public Uri share() {
         //// TODO: check cache folder size
         Bitmap bitmap = Bitmap.createBitmap(Width, Height, Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
@@ -277,9 +279,9 @@ public class DrawView extends View {
             FileOutputStream ostream = new FileOutputStream(file);
             Bitmap.createScaledBitmap(bitmap, outputWidth, outputHeight, true).compress(Bitmap.CompressFormat.JPEG, 75, ostream);
             ostream.close();
-            Log.d("sahre", "share: "+ file.getAbsolutePath());
+            Log.d("sahre", "share: " + file.getAbsolutePath());
 
-            Uri uriToImage = FileProvider.getUriForFile(getContext(), "com.zir.upuptoyou.FileProvider", file );
+            Uri uriToImage = FileProvider.getUriForFile(getContext(), "com.zir.upuptoyou.FileProvider", file);
             return uriToImage;
 
         } catch (Exception e) {
